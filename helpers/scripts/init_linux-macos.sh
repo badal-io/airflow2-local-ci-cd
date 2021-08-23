87a5d76380ed50836ba860a3e8f4668a718756ec
zack.amirakulov <zack.amirakulov@badal.io>
2021-08-23 16:08:59 -0400

    Fix typos

--- helpers/scripts/init_linux-macos.sh
+++ helpers/scripts/init_linux-macos.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 mkdir -p ./logs ./plugins ./data ./dags ./tests/integration ./tests/unit && touch ./variables/docker-env-secrets
 chmod -R 0775 ./logs ./plugins ./data ./dags ./tests
-chmod +x ./airflow ./airflow-gcp-auth
+chmod +x ./airflow ./airflow-gcp
 docker-compose up airflow-init
