012b35e5e9eba75a13566b78acbb8df119ff1c62
zack.amirakulov <zack.amirakulov@badal.io>
2021-08-26 01:02:46 -0400

    Add chmod for execs

--- helpers/scripts/init_linux-macos.sh
+++ helpers/scripts/init_linux-macos.sh
@@ -1,5 +1,4 @@
-
 #!/bin/bash
 mkdir -p ./logs ./plugins ./data ./dags ./tests/integration ./tests/unit && touch ./variables/docker-env-secrets
-chmod -R 0775 ./logs ./plugins ./data ./dags ./tests
+chmod -R 0775 ./logs ./plugins ./data ./dags ./tests ./helpers
 docker-compose up airflow-init
