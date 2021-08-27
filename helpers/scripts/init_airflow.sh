779bbb42309b4e6dad7ea2d75bd600e11d098a1f
zack.amirakulov <zack.amirakulov@badal.io>
2021-08-27 11:52:48 -0400

    Rename the init script

--- helpers/scripts/init_airflow.sh
+++ helpers/scripts/init_airflow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ./logs ./plugins ./data ./dags ./tests/integration ./tests/unit && touch ./variables/docker-env-secrets
-chmod -R 0775 ./logs ./plugins ./data ./dags ./tests ./helpers
+chmod -R 0777 ./logs ./plugins ./data ./dags ./tests ./helpers/scripts
 docker-compose up airflow-init
