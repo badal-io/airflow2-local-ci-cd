7908b0bb4d461967cb5c9536db0b48b0c53f26b3
zack.amirakulov <zack.amirakulov@badal.io>
2021-08-27 14:47:36 -0400

    Add sudo

--- helpers/scripts/docker-wls.sh
+++ helpers/scripts/docker-wls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt update
-apt install -y \
+sudo apt update
+sudo apt install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -14,11 +14,12 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker ${USER}
+su - ${USER}
 
 # Docker-Compose
 curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-docker-compose disable-v2
+sudo chmod +x /usr/local/bin/docker-compose
