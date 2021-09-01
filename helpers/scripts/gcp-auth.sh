2c74589bdb5ee71f0dacb46066ad5fb436ee2a1f
zack.amirakulov <zack.amirakulov@badal.io>
2021-08-31 21:16:09 -0400

    Add project quota disable

--- helpers/scripts/gcp-auth.sh
+++ helpers/scripts/gcp-auth.sh
@@ -1,4 +1,6 @@
 #
 # Run commands in a temporaty ops container with GCP authentification
 #
-exec docker-compose run --rm -e CONNECTION_CHECK_MAX_COUNT=0 airflow-ops bash -c "gcloud auth application-default login"
+
+#exec docker-compose run --rm -e CONNECTION_CHECK_MAX_COUNT=0 airflow-ops bash -c 'gcloud config set project "$GCP_PROJECT_ID" && gcloud auth application-default login --disable-quota-project'
+exec docker-compose run --rm -e CONNECTION_CHECK_MAX_COUNT=0 airflow-ops bash -c 'gcloud auth application-default login --disable-quota-project'
