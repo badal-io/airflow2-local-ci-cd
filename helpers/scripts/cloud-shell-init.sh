31dbe4eb8896ed93cd4e4b02f645e85539d7321f
zack.amirakulov <zack.amirakulov@badal.io>
2021-09-01 12:55:25 -0400

    Modified cloud shell init script

--- helpers/scripts/cloud-shell-init.sh
+++ helpers/scripts/cloud-shell-init.sh
@@ -1,3 +1,3 @@
 cp ./cloud-shell/.customize_environment ../
-sudo chmod +x ./helpers/scripts/init_airflow.sh
-sudo ./helpers/scripts/init_airflow.sh
+sudo chmod +x ./helpers/scripts/init_airflow.sh ./gcp-cloud-shell/.customize_environment
+sudo ./gcp-cloud-shell/.customize_environment
