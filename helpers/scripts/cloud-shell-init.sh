ac8045e5d2ba17352b71e7b3f3630ded8bd63aa8
zack.amirakulov <zack.amirakulov@badal.io>
2021-09-02 15:19:24 -0400

    Add Iac instructions

--- helpers/scripts/cloud-shell-init.sh
+++ helpers/scripts/cloud-shell-init.sh
@@ -1,3 +1,6 @@
-cp ./cloud-shell/.customize_environment ../
+cp ./gcp-cloud-shell/.customize_environment ../
 sudo chmod +x ./helpers/scripts/init_airflow.sh ./gcp-cloud-shell/.customize_environment
 sudo ./gcp-cloud-shell/.customize_environment
+
+#airflow init
+#upload image
