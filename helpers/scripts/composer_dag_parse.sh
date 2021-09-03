033022b8f52560c1cec3fb074bcd15d9e87d3d9a
zack.amirakulov <zack.amirakulov@badal.io>
2021-09-03 11:59:32 -0400

    Add error match strings

--- helpers/scripts/composer_dag_parse.sh
+++ helpers/scripts/composer_dag_parse.sh
@@ -10,11 +10,14 @@ cmd_output=$(</tmp/Output)
 
 # - Error matching string
 error_string1='No data found'
+error_string2='Error'
+error_string3='Broken DAG'
+error_string4='No module'
 
 # - Condifiton to skip to the next step if no error string mathces
-if [[ "$cmd_output" == *"$error_string1"* ]]
+if [[ "$cmd_output" == *"$error_string1"* ]] || [[ "$cmd_output" == *"$error_string2"* ]]  || [[ "$cmd_output" == *"$error_string3"* ]] || [[ "$cmd_output" == *"$error_string4"* ]]
 then
-  echo " >>>>>  NO DAGS PARSED <<<<<<" && exit 1
+  echo " >>>>>  NO DAGS PARSED - see reason(s) below: <<<<<<" && $cmd_output && exit 1
 else
   echo $cmd_output && exit 0
 fi
