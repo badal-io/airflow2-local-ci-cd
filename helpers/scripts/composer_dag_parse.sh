35b2da5c252421c9e7e8bc3dfe4d576ace83ef2e
zack.amirakulov <zack.amirakulov@badal.io>
2021-09-05 21:13:39 -0400

    test

--- helpers/scripts/composer_dag_parse.sh
+++ helpers/scripts/composer_dag_parse.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 # - List DAGs
-gcloud beta composer environments run $1 --location=$2 dags list -- -S /home/airflow/gcs/plugins/test-dags/$3 1> /tmp/Output
+gcloud beta composer environments run $1 --location=$2 dags list -- -S /home/airflow/gcs/plugins/test-dags/$3 &> /tmp/Output
 
 # - Store output string in var
 cmd_output=$(</tmp/Output)
 
 
 # - Error matching string
-declare -a error_string=("No data found" "Error" "Broken DAG" "No module" "Cannot execute subcommand")
-#error_string2='Error'
-#error_string3='Broken DAG'
-#error_string4='No module'
-#error_string5='Cannot execute subcommand'
+error_string1='No data found'
+error_string2='Error'
+error_string3='Broken DAG'
+error_string4='No module'
+error_string5='Cannot execute subcommand'
 
 # - Condifiton to skip to the next step if no error string mathces
 for item in "${error_string[@]}"
 do
-  if [[ "$cmd_output" == *"$item"* ]] # || [[ "$cmd_output" == *"$error_string2"* ]]  || [[ "$cmd_output" == *"$error_string3"* ]] || [[ "$cmd_output" == *"$error_string4"* ]]
+  if [[ "$cmd_output" == *"$item"* ]] || \
+  [[ "$cmd_output" == *"$error_string2"* ]] || \
+  [[ "$cmd_output" == *"$error_string3"* ]] || \
+  [[ "$cmd_output" == *"$error_string4"* ]] || \
+  [[ "$cmd_output" == *"$error_string5"* ]]
   then
     echo " >>>>>  NO DAGS PARSED - see reason(s) below: <<<<<<" && $cmd_output && exit 1
   else
