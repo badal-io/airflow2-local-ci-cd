4ff5601ee58baa06329daab3037c39472fa986fd
zack.amirakulov <zack.amirakulov@badal.io>
2021-09-06 01:07:21 -0400

    test

--- helpers/scripts/composer_dag_send_email.sh
+++ helpers/scripts/composer_dag_send_email.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# - Wait until the DAG is initialized
 n=0
 until [[ $n -ge $4 ]]
 do
@@ -9,5 +10,8 @@ do
   n=$(($n+1))
   sleep $5
 done
+
+# Run the DAG
 gcloud beta composer environments run $1 --location $2 dags trigger -- $3 --conf '{"env":"'${1}'","commit":"'${6}'","branch":"'${7}'"}'
+
 exit $status
