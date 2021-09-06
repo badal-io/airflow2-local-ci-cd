80e0298f0b3590f6cebfa00445a6f4317082d0e8
zack.amirakulov <zack.amirakulov@badal.io>
2021-09-05 22:09:20 -0400

    test

--- helpers/scripts/composer_dag_parse.sh
+++ helpers/scripts/composer_dag_parse.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # - List DAGs
-gcloud beta composer environments run $1 --location=$2 dags list -- -S /home/airflow/gcs/plugins/test-dags/$3 1> /tmp/Output
-#gcloud beta composer environments run $1 --location=$2 dags list -- -S /home/airflow/gcs/plugins/test-dags/$3 >> /tmp/Output 2>&1
+#gcloud beta composer environments run $1 --location=$2 dags list -- -S /home/airflow/gcs/plugins/test-dags/$3 1> /tmp/Output
+gcloud beta composer environments run $1 --location=$2 dags list -- -S /home/airflow/gcs/plugins/test-dags/$3 >> /tmp/Output 2>&1
 
 # - Store output string in var
 cmd_output=$(</tmp/Output)
@@ -16,18 +16,14 @@ error_string4='No module'
 error_string5='Cannot execute subcommand'
 
 # - Condifiton to skip to the next step if no error string mathces
-for item in "${error_string[@]}"
-do
-  if [[ "$cmd_output" == *"$item"* ]] || \
-  [[ "$cmd_output" == *"$error_string2"* ]] || \
-  [[ "$cmd_output" == *"$error_string3"* ]] || \
-  [[ "$cmd_output" == *"$error_string4"* ]] || \
-  [[ "$cmd_output" == *"$error_string5"* ]]
-  then
-    echo " >>>>>  NO DAGS PARSED - see reason(s) below: <<<<<<" && $cmd_output && exit 1
-  else
-    echo $cmd_output && exit 0
-  fi
+if [[ "$cmd_output" == *"$item"* ]] || \
+   [[ "$cmd_output" == *"$error_string2"* ]] || \
+   [[ "$cmd_output" == *"$error_string3"* ]] || \
+   [[ "$cmd_output" == *"$error_string4"* ]] || \
+   [[ "$cmd_output" == *"$error_string5"* ]]
+then
+  echo " >>>>>  NO DAGS PARSED - see reason(s) below: <<<<<<" && $cmd_output && exit 1
+else
+  echo $cmd_output && exit 0
+fi
 done
-
->> /tmp/Output 2>&1
