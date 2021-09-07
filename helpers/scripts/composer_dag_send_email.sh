4c3fbe51584148746a5d8aaafbccada49f01848d
zack.amirakulov <zack.amirakulov@badal.io>
2021-09-07 01:29:31 -0400

    test

--- helpers/scripts/composer_dag_send_email.sh
+++ helpers/scripts/composer_dag_send_email.sh
@@ -5,14 +5,14 @@ n=0
 until [[ $n -ge $4 ]]
 do
   status=0
-  gcloud beta composer environments run $1 --location $2 dags list 2>&1 | grep $3 && break
+  gcloud beta composer environments run $1 --location $2 dags list -- -S /home/airflow/gcs/data/test-dags/ 2>&1 | grep $3 && break
   status=$?
   n=$(($n+1))
   sleep $5
 done
 
 # - Unpause the DAG if paused
-gcloud beta composer environments run $1 --location $2 dags unpause -- $3
+gcloud beta composer environments run $1 --location $2 dags -S /home/airflow/gcs/data/test-dags/ unpause -- $3
 
 # - Run the DAG "semd_email". The email contains commit ID.
 gcloud beta composer environments run $1 --location $2 dags trigger -S /home/airflow/gcs/data/test-dags/ -- $3 --conf '{"key":"'${6}'"}'
