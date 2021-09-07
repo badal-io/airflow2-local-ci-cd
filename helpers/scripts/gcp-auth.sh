39c4fff99965a84a00705774ed58868569a29313
zack.amirakulov <zack.amirakulov@badal.io>
2021-09-06 22:20:40 -0400

    Fix formatting

--- helpers/scripts/gcp-auth.sh
+++ helpers/scripts/gcp-auth.sh
@@ -2,5 +2,4 @@
 # Run commands in a temporaty ops container with GCP authentification
 #
 
-#exec docker-compose run --rm -e CONNECTION_CHECK_MAX_COUNT=0 airflow-ops bash -c 'gcloud config set project "$GCP_PROJECT_ID" && gcloud auth application-default login --disable-quota-project'
 exec docker-compose run --rm -e CONNECTION_CHECK_MAX_COUNT=0 airflow-ops bash -c 'gcloud auth application-default login --disable-quota-project'
