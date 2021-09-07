ab37677df9d19d57094903ad0cf856df4098d099
zack.amirakulov <zack.amirakulov@badal.io>
2021-09-07 14:08:02 -0400

    Add comments

--- helpers/scripts/init_airflow.sh
+++ helpers/scripts/init_airflow.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Airflow init steps
+#
 mkdir -p ./logs ./plugins ./data ./dags ./tests/integration ./tests/unit && \
 touch ./variables/docker-env-secrets ./dags/.airflowignore
 chmod -R 0777 ./logs ./plugins ./data ./dags ./tests ./helpers/scripts
