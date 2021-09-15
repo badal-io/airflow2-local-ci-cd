5a627c4374da468675f0efbefc4d44b85f4fe06e
zack-amirakulov <88668790+zack-amirakulov@users.noreply.github.com>
2021-09-15 15:15:48 -0400

    Add echo for the init command

--- helpers/scripts/cloud-shell-init.sh
+++ helpers/scripts/cloud-shell-init.sh
@@ -1,3 +1,4 @@
 cp ./gcp-cloud-shell/.customize_environment ../
 sudo chmod +x ./helpers/scripts/init_airflow.sh ./gcp-cloud-shell/.customize_environment
 sudo ./gcp-cloud-shell/.customize_environment
+echo "#### - Cloud Shell has been successfully customized, you can proceed with Airflow deployment!"
